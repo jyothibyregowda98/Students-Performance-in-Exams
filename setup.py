1048ef2ddba2740400071f26262d18c185578410
Jyothi-byregowda <jyothi.machenahallibyregowda@truuth.id>
2025-09-13 14:39:40 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,8 +19,8 @@ def get_requirements(file_path:str)->List[str]:
 setup(
 name='mlproject',
 version='0.0.1',
-author='fayyaz',
-author_email='kfayyazsavanur1@gmail.com',
+author='jyothi',
+author_email='jyothibyregowda98@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
 
